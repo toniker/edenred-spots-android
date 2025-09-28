9e01b02c30086657f46aa2f4d8899794af181b15
Antonis Keremidis <tkeremidis@gmail.com>
2025-09-28 23:25:37 +0300

    Lower minimum SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.keremidis.edenredspots"
-        minSdk = 33
+        minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
